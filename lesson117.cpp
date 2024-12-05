2191ed1104cc03a11aeb5d35f14a87db3fdc69b7
Mark Peters <mapeters101@earthlink.net>
2024-12-05 16:47:46 -0500

    Complete lesson117.

--- lesson117.cpp
+++ lesson117.cpp
@@ -1,3 +1,29 @@
+#include <iostream>
+
+class GamePlayer {
+	//  ......
+
+};
+
+
+/*
+int main() {
+	GamePlayer Player1;
 
 
 
+	GamePlayer MainPlayer;
+
+
+
+	GamePlayer AIPlayer;
+
+
+
+	int Age;
+	int Number;
+	int Any;
+
+	return 0;
+}
+*/
