62c358a48d7cbc9b4015dac07ba89287581788e0
Mark Peters <mapeters101@earthlink.net>
2024-12-09 21:21:29 -0500

    Complete lesson122.

--- Lesson122.cpp
+++ Lesson122.cpp
@@ -7,24 +7,33 @@ public:
 	Person();
 	Person(const string& Name);
 
+	~Person();
+
 private:
 	string ClassName{};
 
 };
-
+/*
 int main() {
 	Person NewPerson;
-	Person Finn{ "Finn" };
+	{
+		Person Finn{ "Finn" };
+	}
 
 	return 0;
 }
+*/
 
 Person::Person() {
 	ClassName = "DefaultClass";
+	cout << "Constructor for class " << ClassName << endl;
 }
 
 Person::Person(const string& Name) {
 	ClassName = Name;
+	cout << "Constructor for class " << ClassName << endl;
 }
 
-
+Person::~Person() {
+	cout << "Destructor for class " << ClassName << endl;
+}
