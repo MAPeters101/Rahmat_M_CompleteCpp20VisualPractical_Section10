19814a3ea8b79dface087f0a61f423b4e916bd94
Mark Peters <mapeters101@earthlink.net>
2024-12-09 22:32:25 -0500

    Work on lesson126.

---
+++ lesson126.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+class Person {
+public:
+	Person() : Person( "UserName", 0) {
+
+	}
+
+	Person(const string& NameValue) : Person(NameValue, 0) {
+
+	}
+
+	Person(const string& NameValue, int AgeValue) : 
+		Name{ NameValue }, Age{ AgeValue } {
+
+	}
+
+	string Name{};
+	int Age{};
+
+private:
+
+};
+
+int main() {
+	Person DefaultPerson;
+	Person Bob{ "John" };
+	Person Finn{ "Finn", 21 };
+
+	cout << "Name: " << DefaultPerson.Name << " and age: " << DefaultPerson.Age << endl;
+	cout << "Name: " << Bob.Name << " and age: " << Bob.Age << endl;
+	cout << "Name: " << Finn.Name << " and age: " << Finn.Age << endl;
+
+
+	return 0;
+}
+
+*/
+
