d78990ec218036d855490e5b1f1f1b6643aa5a44
Mark Peters <mapeters101@earthlink.net>
2024-12-10 11:07:57 -0500

    Work on lesson128.

---
+++ lesson128.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+class Cube {
+public:
+	explicit Cube(int CubeSide1) : CubeSide{ CubeSide1 } {};
+
+	int CubeVolume() {
+		return CubeSide * CubeSide * CubeSide;
+	}
+
+	bool bCubeVolumeLargerThan(Cube NewCube) {
+		return CubeVolume() > NewCube.CubeVolume();
+	}
+
+private:
+	int CubeSide{};
+};
+
+
+int main() {
+
+	Cube Cube1(5);
+	Cube Cube2(2);
+
+	bool bGreater = Cube1.bCubeVolumeLargerThan(Cube2);
+	//bool bGreater1 = Cube1.bCubeVolumeLargerThan(13);
+
+	cout << "Results1: " << bGreater << endl;
+	cout << "Results2: " << bGreater1 << endl;
+
+
+	return 0;
+}
+
+*/
+
