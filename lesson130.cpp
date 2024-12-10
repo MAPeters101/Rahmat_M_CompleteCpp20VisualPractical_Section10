75a03ce8a708c7afcb5da8d0597c77b08b7094ca
Mark Peters <mapeters101@earthlink.net>
2024-12-10 12:02:22 -0500

    Work on lesson131.

---
+++ lesson130.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+class Person {
+public:
+	Person(const string& NameValue, int AgeValue) {
+		Age = new int;
+		*Age = AgeValue;
+	}
+	Person(const Person& Other) : Age{ Other.Age } {
+	}
+
+	~Person() {
+		delete Age;
+	}
+
+	string Name{};
+	int* Age{};
+
+private:
+
+};
+
+
+int main() {
+
+	Person Finn{ "Finn", 21 };
+	cout << Finn.Age << endl;
+	cout << *Finn.Age << endl;
+
+	{
+		Person CopyConstructor{ Finn };
+		cout << CopyConstructor.Age << endl;
+		cout << *CopyConstructor.Age << endl;
+	}
+
+	cout << Finn.Age << endl;
+	cout << *Finn.Age << endl;
+
+
+	return 0;
+}
+
+*/
+
