8f9ff814f6faf78ef2b573f14438bb40eaddcb94
Mark Peters <mapeters101@earthlink.net>
2024-12-10 11:20:09 -0500

    Work on lesson129.

---
+++ lesson129.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+class Person {
+public:
+	Person(const string& NameValue, int AgeValue) : Name{ NameValue }, Age{ AgeValue } {
+
+	}
+
+	//Person(const Person& Other) = delete;
+
+	//Person(const Person& Other) : Name{ Other.Name }, Age{ Other.Age } {
+	//}
+
+	Person(const Person& Other) : Person( Other.Name, Other.Age) {
+	}
+
+
+private:
+	string Name{};
+	int Age{};
+};
+
+
+int main() {
+	Person Finn{ "Finn", 21 };
+	Person CopyFinn{ Finn };
+
+	return 0;
+}
+
+*/
