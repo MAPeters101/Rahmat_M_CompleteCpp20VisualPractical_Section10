cf239c29db3303890db807dad3836eb68a95be0c
Mark Peters <mapeters101@earthlink.net>
2024-12-09 20:47:45 -0500

    Work on lesson120.

---
+++ lesson120.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+using namespace std;
+
+struct Person {
+	string Name{};
+	int Age{};
+};
+/*
+int main() {
+	Person Finn;
+	Person Tony;
+
+	Finn.Name = "Finn";
+	Finn.Age = 70;
+
+	Tony.Name = "Tony";
+	Tony.Age = 50;
+
+	cout << Finn.Name << " " << Finn.Age << endl;
+	cout << Tony.Name << " " << Tony.Age << endl;
+
+}
+*/
+
+
